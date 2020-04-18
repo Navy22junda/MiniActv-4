3f7cb73b7154c0e9b2a4e0e8fc49388c48ff6806
Navy22junda <navy22junda@gmail.com>
2020-04-18 09:54:42 +0200

    Ús de servei et deixa elegir entre si vols reproduir musica o soroll, o els 2 o parar

--- ManejoServicio-master/app/src/main/java/com/android/mdw/demo/Main.java
+++ ManejoServicio-master/app/src/main/java/com/android/mdw/demo/Main.java
@@ -8,31 +8,41 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Main extends Activity implements OnClickListener {
-  private Intent in;
+    private Intent in;
+    Button btnSonido;
+    Button btnCancion;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-    Button btnInicio = (Button) findViewById(R.id.btnInicio);
-    Button btnFin = (Button) findViewById(R.id.btnFin);
+        Button btnFin = (Button) findViewById(R.id.btnFin);
 
-    btnInicio.setOnClickListener(this);
-    btnFin.setOnClickListener(this);
+        btnSonido = (Button)findViewById(R.id.btnSonido);
+        btnCancion = (Button)findViewById(R.id.btnCancion);
 
-    in = new Intent(this, ElServicio.class);
-  }
+        btnSonido.setOnClickListener(this);
+        btnCancion.setOnClickListener(this);
+        btnFin.setOnClickListener(this);
 
-  @Override
-  public void onClick(View src) {
-    switch (src.getId()) {
-    case R.id.btnInicio:      
-      startService(in);
-      break;
-    case R.id.btnFin:
-      stopService(in);
-      break;
+        in = new Intent(this, ElServicio.class);
     }
-  }
+
+    @Override
+    public void onClick(View src) {
+        switch (src.getId()) {
+            case R.id.btnSonido:
+                in.putExtra("opcio", btnSonido.getText());
+                startService(in);
+                break;
+            case R.id.btnCancion:
+                in.putExtra("opcio", btnCancion.getText());
+                startService(in);
+                break;
+            case R.id.btnFin:
+                stopService(in);
+                break;
+        }
+    }
 }
